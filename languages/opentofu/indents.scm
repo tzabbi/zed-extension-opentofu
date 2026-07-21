5b0a8123a7d2af08b9fee2f780d25d29d3cade7c
tzabbi <tom@zaspel.it>
2026-07-21 12:48:49 +0200

    fix: tree-sitter gramma indentation for each loop new curly brackets

--- languages/opentofu/indents.scm
+++ languages/opentofu/indents.scm
@@ -4,6 +4,8 @@
   (object)
   (tuple)
   (function_call)
+  (for_object_expr)
+  (for_tuple_expr)
 ] @indent
 
 [
@@ -11,6 +13,5 @@
   "}"
   ")"
 ] @outdent
-
 ; https://github.com/nvim-treesitter/nvim-treesitter/blob/ce4adf11cfe36fc5b0e5bcdce0c7c6e8fbc9798a/queries/terraform/indents.scm
 ; inherits: hcl
